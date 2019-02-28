4d50b20728d7d912c511c016a3cfc2a1c90963a9
dev-ritik <ritikkne@gmail.com>
2019-02-28 14:45:51 +0530

    Adds server and client codes

---
+++ client.cpp
@@ -0,0 +1,50 @@
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#define PORT 8000 
+   
+int main(int argc, char const *argv[]) 
+{ 
+    struct sockaddr_in address; 
+    int sock = 0, valread; 
+    struct sockaddr_in serv_addr; 
+    char buffer[1024] = {0}; 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    { 
+        printf("\n Socket creation error \n"); 
+        return -1; 
+    } 
+   
+    memset(&serv_addr, '0', sizeof(serv_addr)); 
+   
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORT); 
+       
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    } 
+   
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("\nConnection Failed \n"); 
+        return -1; 
+    } 
+    char str [80];
+
+    while(true){
+        printf ("Enter text: ");
+        scanf ("%s",str);  
+        printf ("Sending %s \n",str);
+        
+        send(sock , str , strlen(str) , 0 ); 
+        valread = read( sock , buffer, 1024); 
+        printf("%s\n",buffer ); 
+    }
+}
